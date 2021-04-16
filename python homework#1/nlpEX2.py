d6e8cf0882edb334f6fbc6f4bc3f5d1faa222d04
Beeplosion <sao5303@psu.edu>
2021-04-16 12:49:30 -0400

    f

--- python homework#1/nlpEX2.py
+++ python homework#1/nlpEX2.py
@@ -10,27 +10,30 @@ print(lemmatizer.mode)  # 'rule'
 KHsp =open('pythonHW_1_sampleTEXT_temp.txt', 'r', encoding="utf8",errors='ignore')
 speeches = KHsp.read()
 KHspeeches = nlp(speeches)
+print(KHspeeches)
 
 def verbcollector(speeches):
     Verbs = []
     count = 0
     for token in speeches:
-        if token.pos_ =="VERB":
+        if token.pos_ == "VERB":
             count += 1
             Verbs.append(token.lemma_)
             print(count, ": ", token, token.pos_, spacy.explain(token.pos_))
-        return Verbs
+    return Verbs
 
 listVerbs = verbcollector(KHspeeches)
+print(listVerbs)
 verb_freq = Counter(listVerbs)
 topTen = verb_freq.most_common(10)
 print(topTen)
-lasTen = verb_freq.most_common()[:-5:-1]
+lastTen = verb_freq.most_common()[:-5:-1]
+print(lastTen)
 
 bar_chartOver10 = pygal.Bar()
-bar_chartTopTen = pygal.Bar()
-
 bar_chartOver10.title = 'Verbs Used Over 10 times in Disney Songs'
+
+bar_chartTopTen = pygal.Bar()
 bar_chartTopTen.title = 'Top 10 Verbs in Kingdom Hearts'
 
 for v in verb_freq:
